9098bb84aab0c700f7c6c8c896b7df99a584f317
gitsolsol <shahdahmed7203@gmail.com>
2025-05-24 00:55:18 +0300

    driver has a wobbly head

--- Assets/Scripts/DriveCar.cs
+++ Assets/Scripts/DriveCar.cs
@@ -6,7 +6,9 @@ public class DriveCar : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _frontTireRB;
     [SerializeField] private Rigidbody2D _backTireRB;
+    [SerializeField] private Rigidbody2D _carRB;
     [SerializeField] private float _speed = 150f;
+    [SerializeField] private float _rotationSpeed = 300f;
 
     private float _moveInput;
 
@@ -26,5 +28,6 @@ public class DriveCar : MonoBehaviour
     {
         _frontTireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
         _backTireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
+        _carRB.AddTorque(_moveInput * _rotationSpeed * Time.fixedDeltaTime);
     }
 }
