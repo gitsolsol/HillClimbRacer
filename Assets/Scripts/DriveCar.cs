a25c7a51892da5527444dc4ead85ecf7009c8836
gitsolsol <shahdahmed7203@gmail.com>
2025-05-23 23:42:02 +0300

    car moves

---
+++ Assets/Scripts/DriveCar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveCar : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _frontTireRB;
+    [SerializeField] private Rigidbody2D _backTireRB;
+    [SerializeField] private float _speed = 150f;
+
+    private float _moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _moveInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        _frontTireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
+        _backTireRB.AddTorque(-_moveInput * _speed * Time.fixedDeltaTime);
+    }
+}
